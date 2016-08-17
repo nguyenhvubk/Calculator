c4b38d851e2309a19003d9cb360d23f7531c45ef
Hoang Vu <nguyenhvubk@gmail.com>
2016-08-17 20:01:36 +0900

    Original version

--- 6.Caculator/ViewController.swift
+++ 6.Caculator/ViewController.swift
@@ -7,19 +7,100 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operator: String {
+        case Devide = "/"
+        case Multiple = "*"
+        case Plus = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var OutputLabel: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperator: Operator = Operator.Empty
+    var Result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func ButtonPressed (btn: UIButton!){
+        playSound()
+        runningNumber += "\(btn.tag)"
+        OutputLabel.text = runningNumber
     }
-
-
+    
+    @IBAction func DevidePressed (sender: AnyObject) {
+        processOperator(Operator.Devide)
+    }
+    @IBAction func MultiplyPressed(sender: AnyObject) {
+        processOperator(Operator.Multiple)
+    }
+    @IBAction func PlusPressed(sender: AnyObject) {
+        processOperator(Operator.Plus)
+    }
+    @IBAction func SubtractPressed(sender: AnyObject) {
+        processOperator(Operator.Subtract)
+    }
+    @IBAction func EqualsPressed(sender: AnyObject) {
+        processOperator(currentOperator)
+    }
+    
+    func processOperator (op: Operator) {
+        playSound()
+        
+        if currentOperator != Operator.Empty {
+            if runningNumber != ""{
+            rightValStr = runningNumber
+            runningNumber = ""
+            if currentOperator == Operator.Devide {
+                Result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+            } else if currentOperator == Operator.Multiple {
+                Result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+            } else if currentOperator == Operator.Plus {
+                Result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+            } else if currentOperator == Operator.Subtract {
+                Result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+            }
+            
+            leftValStr = Result
+            OutputLabel.text = Result
+            
+        }
+            currentOperator = op
+            
+        } else {
+            //First Operator ever
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperator = op
+            
+        }
+    }
+    
+    func playSound () {
+        if btnSound.playing{
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
+    
 }
 
